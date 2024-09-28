ff03003d5d41e59c078334ca5ef1e9cd115d477d
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-09-28 09:01:03 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     float num1, num2, massimo;
-    cout<<"Scrivi il primo numero."<<num1<<endl;
+    cout<<"Scrivi il primo numero."<<endl;
     cin>>num1;
-    cout<<"Scrivi il secondo numero."<<num2<<endl;
+    cout<<"Scrivi il secondo numero."<<endl;
     cin>>num2;
     if(num1>num2)
     {
